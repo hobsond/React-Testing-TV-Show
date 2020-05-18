27fe5593dd4777f56f04922d6958dae081ae5610
unknown <hobsond26@icloud.com>
2020-05-18 16:21:49 -0400

    created api file and transfered the function fetchShow

---
+++ src/api/fetchShow.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import axios from 'axios'
+
+const fetchShow = (setShow,setSeasons,formatSeasons) => {
+    axios
+      .get(
+        "https://api.tvmaze.com/singlesearch/shows?q=stranger-things&embed=episodes"
+      )
+      .then(res => {
+        setShow(res.data);
+        setSeasons(formatSeasons(res.data._embedded.episodes));
+      });
+  };
+
+  export default fetchShow
